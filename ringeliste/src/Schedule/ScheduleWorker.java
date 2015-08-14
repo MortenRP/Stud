164ddd3e2efe961f424559ac5cc235a860994dee
Martin Kristjansen <martinkri35@gmail.com>
2015-08-14 13:26:07 +0200

    Added skeleton to the ScheduleWorker so the most function headings are done.

--- ringeliste/src/Schedule/ScheduleWorker.java
+++ ringeliste/src/Schedule/ScheduleWorker.java
@@ -1,7 +1,8 @@
 package Schedule;
 
 import User.JobFunction;
-import jxl.*;
+import User.User;
+import jxl.Sheet;
 
 import java.util.HashMap;
 
@@ -10,9 +11,26 @@ import java.util.HashMap;
  */
 public class ScheduleWorker {
 
-    private HashMap<String, Integer> coloumsByName;
+    private HashMap<String, Integer> columnsByName;
+    private HashMap<JobFunction, PhoneList> phoneListsByJobFunction;
+    private Sheet sheet;
+    
+    public ScheduleWorker(Sheet sheet){
+        this.sheet = sheet;
 
-    public ScheduleWorker(Sheet sheet, JobFunction jobFunction){
+        columnsByName = findColumns();
+        phoneListsByJobFunction = generatePhoneLists();
+    }
 
+    public PhoneList getPhoneListForFunction(JobFunction jobFunction){
+        return null;
+    }
+
+    private HashMap<String, Integer> findColumns(){
+        return null;
+    }
+
+    private HashMap<JobFunction, PhoneList> generatePhoneLists(){
+        return null;
     }
 }

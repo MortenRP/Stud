cadd68e14771d822c120647f84641afb40d4aa69
Martin Kristjansen <martinkri35@gmail.com>
2015-09-01 13:02:39 +0200

    Added the personnel class for this branch.

---
+++ ringeliste/src/User/Personnel.java
@@ -0,0 +1,45 @@
+package User;
+
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * Created by GameMonkey on 26-08-2015.
+ */
+public class Personnel {
+
+    private HashMap<Integer, User> usersByID;
+
+    private Personnel(){
+        usersByID = new HashMap<>();
+    }
+
+    //Static make method for a more describing name
+    // @ToDo Find another name for variable personnel
+    public static Personnel makeFromPersonnelFiles(String personnel, String salary){
+        Personnel staff = new Personnel();
+
+
+        return new Personnel();
+    }
+
+
+    //@ToDo Add error handling if user is in system already.
+    private void addUser(String name, int ID, JobFunction jobFunction){
+
+    }
+
+    public void updateUserDate(int ID, Date date){
+
+    }
+
+    private int getHoursWorkedOfUser(int ID){
+        return 0;
+    }
+
+    private JobFunction getGroupOfUser(int ID){
+        return null;
+    }
+
+
+}
